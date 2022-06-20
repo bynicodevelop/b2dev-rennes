cef04264d6a6152ee48768687f11f5778ca94027
nico-develop <contact@nico-develop.fr>
2022-06-20 15:28:18 +0200

    Added Page View with navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/screens/add_screen.dart';
-import 'package:flutter_application_2/screens/no_return_screen.dart';
+import 'package:flutter_application_2/screens/page_views/feed_screen.dart';
+import 'package:flutter_application_2/screens/page_views/profiles_screen.dart';
 import 'package:flutter_application_2/screens/profile_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController(
+    initialPage: 0,
+  );
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -32,47 +43,63 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        // width: screenSize.width * .6, // width: 100%
-        width: screenSize.width,
-        color: Colors.amber,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  FontAwesomeIcons.facebook,
-                  color: Colors.blue,
-                ),
-                Icon(
-                  FontAwesomeIcons.twitter,
-                  color: Colors.blue,
-                ),
-                Icon(
-                  FontAwesomeIcons.instagram,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-            const Text("Coucou les devs"),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const NoReturnScreen(),
-                  ),
-                  (route) => false,
-                );
-              },
-              child: const Text("Coucou les devs 2"),
-            ),
-          ],
-        ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (value) {
+          print("Page changed: $value");
+
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        scrollDirection: Axis.horizontal,
+        children: const [
+          FeedScreen(),
+          ProfilesScreen(),
+        ],
       ),
+
+      // body: Container(
+      //   // width: screenSize.width * .6, // width: 100%
+      //   width: screenSize.width,
+      //   color: Colors.amber,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         children: const [
+      //           Icon(
+      //             FontAwesomeIcons.facebook,
+      //             color: Colors.blue,
+      //           ),
+      //           Icon(
+      //             FontAwesomeIcons.twitter,
+      //             color: Colors.blue,
+      //           ),
+      //           Icon(
+      //             FontAwesomeIcons.instagram,
+      //             color: Colors.blue,
+      //           ),
+      //         ],
+      //       ),
+      //       const Text("Coucou les devs"),
+      //       ElevatedButton(
+      //         onPressed: () {
+      //           Navigator.pushAndRemoveUntil(
+      //             context,
+      //             MaterialPageRoute(
+      //               builder: (context) => const NoReturnScreen(),
+      //             ),
+      //             (route) => false,
+      //           );
+      //         },
+      //         child: const Text("Coucou les devs 2"),
+      //       ),
+      //     ],
+      //   ),
+      // ),
       // body: const Center(
       //   child: Text(
       //     'Hello World',
@@ -99,6 +126,7 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -109,6 +137,21 @@ class HomeScreen extends StatelessWidget {
             label: 'Search',
           ),
         ],
+        onTap: (value) {
+          print("On Tap: $value");
+
+          setState(() {
+            _currentIndex = value;
+          });
+
+          _pageController.animateToPage(
+            value,
+            duration: const Duration(
+              milliseconds: 300,
+            ),
+            curve: Curves.easeInOut,
+          );
+        },
       ),
     );
   }
